aeb7571ad44b28bb3ba7a2e12af0e6b332c35f2c
PetrovYegor <YegorPetrov@outlook.com>
2025-04-13 22:27:24 +0300

    Класс ActionUtils использовался и как класс констант, и как утильный класс. Убрал коснтанты, класс теперь только утильный

--- src/simulation/Main.java
+++ src/simulation/Main.java
@@ -14,18 +14,23 @@ public class Main {
     private static final int GAMEBOARD_WIDTH = 10;
     private static final int GRASS_GENERATE_VALUE = 3;
     private static final int HERBIVORE_GENERATE_VALUE = 2;
+    private static final int HERBIVORE_LIMIT = 3;
+    private static final int GRASS_LIMIT = 5;
+    private static final int ROCK_LIMIT = 3;
+    private static final int PREDATOR_LIMIT = 1;
+    private static final int TREE_LIMIT = 5;
 
     public static void main(String[] args) {
         GameBoard board = new GameBoard(GAMEBOARD_HEIGHT, GAMEBOARD_WIDTH);
         List<Action> initActions = List.of(
-                new SpawnAction<>(board, ActionUtils.PREDATOR_LIMIT, () -> new Predator(BoardUtils.getRandomFreeCoordinates(board, board.getHeight(), board.getWidth()), ActionUtils.getRandomSpeed(), ActionUtils.getRandomHealth(), ActionUtils.getRandomAttackPower())),
-                new SpawnAction<>(board, ActionUtils.HERBIVORE_LIMIT
+                new SpawnAction<>(board, PREDATOR_LIMIT, () -> new Predator(BoardUtils.getRandomFreeCoordinates(board, board.getHeight(), board.getWidth()), ActionUtils.getRandomSpeed(), ActionUtils.getRandomHealth(), ActionUtils.getRandomAttackPower())),
+                new SpawnAction<>(board, HERBIVORE_LIMIT
                         , () -> new Herbivore(BoardUtils.getRandomFreeCoordinates(board, board.getHeight(), board.getWidth())
                         , ActionUtils.getRandomSpeed()
                         , ActionUtils.getRandomHealth())),
-                new SpawnAction<>(board, ActionUtils.GRASS_LIMIT, Grass::new),
-                new SpawnAction<>(board, ActionUtils.ROCK_LIMIT, Rock::new),
-                new SpawnAction<Tree>(board, ActionUtils.TREE_LIMIT, Tree::new)
+                new SpawnAction<>(board, GRASS_LIMIT, Grass::new),
+                new SpawnAction<>(board, ROCK_LIMIT, Rock::new),
+                new SpawnAction<Tree>(board, TREE_LIMIT, Tree::new)
         );
         List<Action> turnActions = List.of(
                 new RechargeAction<Grass>(board, Grass.class, Grass::new, GRASS_GENERATE_VALUE),
--- src/simulation/actions/ActionUtils.java
+++ src/simulation/actions/ActionUtils.java
@@ -12,12 +12,6 @@ public final class ActionUtils {
     private static final int MIN_ATTACK_POWER_VALUE_EXCLUSIVE = 1;
     private static final int MAX_ATTACK_POWER_VALUE_EXCLUSIVE = 3;
 
-    public static final int HERBIVORE_LIMIT = 3;
-    public static final int GRASS_LIMIT = 5;
-    public static final int ROCK_LIMIT = 3;
-    public static final int PREDATOR_LIMIT = 1;
-    public static final int TREE_LIMIT = 5;
-
     private static final Random random = new Random();
 
     public static int getRandomHealth() {
